6aac27057693e3432fd586e95a846fb28ca13bd5
Laura Díaz Vera <valentina.hp@gmail.com>
2017-03-27 16:09:26 -0300

    Production mailing

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -17,7 +17,21 @@ Rails.application.configure do
   # Disable serving static files from the `/public` folder by default since
   # Apache or NGINX already handles this.
   config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present?
+  
   config.action_mailer.default_url_options = { :host => 'atumedida.herokuapp.com' }
+  config.action_mailer.perform_caching = false
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.default :charset => "utf-8"
+  
+  ActionMailer::Base.smtp_settings = {
+    :address => "smtp.gmail.com",
+    :port => 587,
+    :authentication => :login,
+    :domain => 'gmail.com',
+    :user_name => ENV['email'],
+    :password => ENV['email_password'],
+  }
   # Compress JavaScripts and CSS.
   config.assets.js_compressor = :uglifier
   # config.assets.css_compressor = :sass
